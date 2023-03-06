7f031ab3cc8e3d310788216e3658c097eaf2f0c0
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-03-06 21:38:09 +0300

    Add exercise 32

---
+++ chapter1/exercise32/sum_of_digits_in_number.py
@@ -0,0 +1,10 @@
+number = int(input("Введите целое четырёхзначное число: "))
+
+thousands = number // 1000
+hundreds = (number - thousands * 1000) // 100
+tens = (number - thousands * 1000 - hundreds * 100) // 10
+remainder = number - thousands * 1000 - hundreds * 100 - tens * 10
+
+amount = thousands + hundreds + tens + remainder
+
+print(f"Сумма цифра числа {number} равна {amount}")
