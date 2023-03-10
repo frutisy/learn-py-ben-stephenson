3e4f887c208d02ebba546be679744df0cacde644
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-03-10 17:26:20 +0300

    Add exercise 46

---
+++ chapter2/exercise46/сhinese_horoscope.py
@@ -0,0 +1,26 @@
+birthday = int(input("Введите год вашего рождения (например, '2000'): "))
+
+if birthday % 12 == 0:
+    print("Вы родились в год Обезьяны")
+elif birthday % 12 == 1:
+    print("Вы родились в год Петуха")
+elif birthday % 12 == 2:
+    print("Вы родились в год Собаки")
+elif birthday % 12 == 3:
+    print("Вы родились в год Свиньи")
+elif birthday % 12 == 4:
+    print("Вы родились в год Мыши")
+elif birthday % 12 == 5:
+    print("Вы родились в год Быка")
+elif birthday % 12 == 6:
+    print("Вы родились в год Тигра")
+elif birthday % 12 == 7:
+    print("Вы родились в год Кролика")
+elif birthday % 12 == 8:
+    print("Вы родились в год Дракона")
+elif birthday % 12 == 9:
+    print("Вы родились в год Змеи")
+elif birthday % 12 == 10:
+    print("Вы родились в год Лошади")
+elif birthday % 12 == 11:
+    print("Вы родились в год Овцы")
