63979f837db43fa03595a9f48c083cda60bb3ee2
Vladimir Strukov <ise.vladimir.strukov@gmail.com>
2023-04-05 12:25:43 +0300

    Add exercise 86

---
+++ chapter6/exercise86/morse_code.py
@@ -0,0 +1,42 @@
+from string import ascii_uppercase
+
+symbols = ascii_uppercase + '0123456789'
+codes = ('.–', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..',
+         '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.',
+         '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', '-----',
+         '.----', '..---', '...--', '....-', '.....', '-....', '--...',
+         '---..', '----.'
+         )
+
+MORSE_CODE = dict(zip(symbols, codes))
+
+
+def convert_string_to_morse_code(text: str) -> str:
+    """
+    Преобразует переданную строку в последовательность точек и тире,
+    вставляя пробелы между отдельными словами.
+
+    Символы, которые не находятся в словаре MORSE_CODE будут игнорироваться."""
+    morse_string = ''
+
+    for symbol in text.upper():
+        if symbol not in MORSE_CODE.keys():
+            continue
+        else:
+            morse_string += MORSE_CODE[symbol] + ' '
+
+    return morse_string
+
+
+def show_morse_code(text: str) -> None:
+    """Выводит строку, преобразованную с помощью азбуки Морзе."""
+    print(convert_string_to_morse_code(text))
+
+
+def main():
+    text = input("Введите строку: ")
+    show_morse_code(text)
+
+
+if __name__ == '__main__':
+    main()
